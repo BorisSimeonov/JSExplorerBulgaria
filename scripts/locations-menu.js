c01a69a53ac4ff9958c1c761ba0217479eec77cd
twistbobg@gmail.com <twistbobg@gmail.com>
2016-11-20 23:12:42 +0200

    - Article Display added. - Fixed bug with multiple <select> tags visualisation after repeated clicks on location item. - minimized HTML content and optimized css file - Two new fonts added (DroidSerif, DroidSerif-BoldItalic)

--- scripts/locations-menu.js
+++ scripts/locations-menu.js
@@ -6,10 +6,12 @@ function loadLocations() {
         base64auth = btoa(`${username}:${password}`),
         authHeaders = {"Authorization": "Basic " + base64auth},
         locationsList = $('#locations-list'),
-        articleContainer = $('#article-content'),
-        selectContainer = $('#select-container');
+        articleHolder = $('#article-holder'),
+        locationSelectDropDown = $('#article-selector'),
+        trailImagesCropSize = 40;
 
-    locationsList.on('click', '.location-item', displayArticles);
+    locationsList.on('click', '.location-item', listArticlesOptions);
+    locationSelectDropDown.on('change', loadArticle);
 
     function requestLocations() {
         $.get({
@@ -36,9 +38,8 @@ function loadLocations() {
         console.log(`Error: ${error.status} (${error.statusText})`);
     }
 
-    function displayArticles() {
-        articleContainer.empty();
-        selectContainer.empty();
+    function listArticlesOptions() {
+        articleHolder.empty();
         let location_id = $(this).attr('location_id');
 
         $.get({
@@ -50,22 +51,91 @@ function loadLocations() {
     }
 
     function appendArticleTitlesAsOptions(articles) {
-        let select = $('<select>')
-            .attr('id', 'article-selector');
-        select.append($('<option>')
+        locationSelectDropDown.empty();
+        let defaultOption = $('<option>')
             .val('#')
-            .text('No selection')
             .attr('selected', 'true')
-            .attr('disabled', 'disabled'));
+            .attr('disabled', 'disabled'),
+            len = articles.length;
 
-        for(let article of articles) {
-            select.append($('<option>')
+        if (len) {
+            defaultOption.text(`Select article: ${len} results`);
+        } else {
+            defaultOption.text('No Articles Found');
+        }
+
+        locationSelectDropDown.append(defaultOption);
+
+        for (let article of articles) {
+            locationSelectDropDown.append($('<option>')
                 .val(article._id)
                 .text(article.title)
             );
         }
-        select.select(() => console.log('selected/changed'));
-        selectContainer.append(select);
+    }
+
+    function loadArticle() {
+        let selectedArticleId = $(this).find(':selected').val();
+
+        $.get({
+            url: articlesUrl + `?query={"_id":"${selectedArticleId}"}`,
+            headers: authHeaders
+        })
+            .then(drawArticle)
+            .catch(handleError);
+    }
+
+    function drawArticle(article) {
+        articleHolder.empty();
+        article = article[0];
+        let articleHTML = $('<article>')
+            .attr('article_id', String(article._id));
+        articleHTML
+            .append($('<h1>')
+                .addClass('article-header')
+                .text(article.title))
+            .append($('<img>')
+                    .addClass('lead-image')
+                    .attr('src', article.leading_image_url)
+                .attr('alt','No Image'))
+            .append($('<p>')
+                .addClass('article-description')
+                .text(article.description))
+            .append($('<p>')
+                .addClass('article-main-text')
+                .text(article.text));
+
+        let imageLocations = article.trailing_images.split(',');
+        imageLocations.forEach((img_link) => {
+            articleHTML.append($('<div class="image-wrapper">').append($('<img>')
+                .addClass('trail-image')
+                .attr('src', img_link)
+                .attr('alt', 'No Image'))
+                .on('click', imageResizeOnClick));
+        });
+
+        articleHolder.append(articleHTML);
+    }
+
+    function imageResizeOnClick() {
+        let imageWrapper = $(this);
+        if(imageWrapper.width() === trailImagesCropSize) {
+            imageWrapper
+                .css('width', '90%')
+                .css('height', 'auto')
+                .css('margin-left', 'auto')
+                .css('margin-right', 'auto')
+                .css('display', 'inherit');
+        } else {
+            imageWrapper
+                .css('width', '40px')
+                .css('height', '40px')
+                .css('margin-left', '10px')
+                .css('margin-right', '10px')
+                .css('display', 'inline-table');
+        }
+
+
     }
 
     requestLocations();
