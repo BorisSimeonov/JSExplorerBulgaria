fb5b458a5c676c381cdf5a3d0fec32976f0654eb
Bob Source <twistbobg@gmail.com>
2016-11-21 16:21:38 +0200

    new: - Navigation Menu with handled async requests visualisation - Function reduced coupling for requests - Added "villages" section - Modernized design (brighter tones)

--- scripts/locations-menu.js
+++ scripts/locations-menu.js
@@ -1,61 +1,79 @@
 function loadLocations() {
-    const locationsUrl = 'https://baas.kinvey.com/appdata/kid_HJiZXC6bx/locations',
-        articlesUrl = 'https://baas.kinvey.com/appdata/kid_HJiZXC6bx/articles',
+    const baseUrl = 'https://baas.kinvey.com/appdata/kid_HJiZXC6bx',
+        mountainsUrl = baseUrl + '/locations',
+        municipalityUrl = baseUrl + '/municipality',
+        articlesUrl = baseUrl + '/articles',
         username = 'test',
         password = 'test',
         base64auth = btoa(`${username}:${password}`),
         authHeaders = {"Authorization": "Basic " + base64auth},
-        locationsList = $('#locations-list'),
+        mainList = $('#main-locations'),
+        mountainLocationsListId = 'mountain-locations-list',
+        mountainList = $(`#${mountainLocationsListId}`),
+        municipalityLocationsListId = 'village-locations-list',
+        municipalityList = $(`#${municipalityLocationsListId}`),
         articleHolder = $('#article-holder'),
         locationSelectDropDown = $('#article-selector'),
         trailImagesCropSize = 40;
 
-    locationsList.on('click', '.location-item', listArticlesOptions);
+
+    mainList.on('click', '.location-item', listArticlesOptions);
+    mainList.on('click', 'img', toggleDOMElements);
     locationSelectDropDown.on('change', loadArticle);
 
-    function requestLocations() {
+    function requestLocations(tableUrl, destinationList) {
         $.get({
-            url: locationsUrl,
+            url: tableUrl,
             headers: authHeaders
         })
-            .then(appendLocations)
+            .then((locationArray) =>
+                appendLocationItem.call(this, locationArray, destinationList))
             .catch(handleError)
     }
 
-    function appendLocations(locationsArray) {
+    function appendLocationItem(locationsArray, destination) {
         locationsArray.forEach(location => {
-            let newLocation = $('<li>')
-                .append($('<a>')
-                    .attr('href', '#')
-                    .addClass('location-item')
-                    .text(location.locationName)
-                    .attr('location_id', location._id));
-            locationsList.append(newLocation);
+            if (checkIfLocationExists(location._id, destination)) {
+                let newLocation = $('<li>')
+                    .append($('<a>')
+                        .attr('href', '#')
+                        .addClass('location-item')
+                        .text(location.locationName)
+                        .attr('location_id', location._id));
+                destination.append(newLocation);
+            }
         })
+    }
+
+    function checkIfLocationExists(id, target) {
+        let idCount = target
+            .children(`li:has(a[location_id="${id}"])`).length;
+        return idCount ? false : true;
     }
 
     function handleError(error) {
         console.log(`Error: ${error.status} (${error.statusText})`);
     }
 
-    function listArticlesOptions() {
+    function listArticlesOptions(event) {
         articleHolder.empty();
         let location_id = $(this).attr('location_id');
-
         $.get({
             url: articlesUrl + `?query={"location_id":"${location_id}"}`,
             headers: authHeaders
         })
             .then(appendArticleTitlesAsOptions)
             .catch(handleError);
+        event.stopPropagation();
+        event.preventDefault();
     }
 
     function appendArticleTitlesAsOptions(articles) {
         locationSelectDropDown.empty();
         let defaultOption = $('<option>')
-            .val('#')
-            .attr('selected', 'true')
-            .attr('disabled', 'disabled'),
+                .val('#')
+                .attr('selected', 'true')
+                .attr('disabled', 'disabled'),
             len = articles.length;
 
         if (len) {
@@ -95,9 +113,9 @@ function loadLocations() {
                 .addClass('article-header')
                 .text(article.title))
             .append($('<img>')
-                    .addClass('lead-image')
-                    .attr('src', article.leading_image_url)
-                .attr('alt','No Image'))
+                .addClass('lead-image')
+                .attr('src', article.leading_image_url)
+                .attr('alt', 'No Image'))
             .append($('<p>')
                 .addClass('article-description')
                 .text(article.description))
@@ -105,7 +123,9 @@ function loadLocations() {
                 .addClass('article-main-text')
                 .text(article.text));
 
-        let imageLocations = article.trailing_images.split(',');
+        let imageLocations = article.trailing_images
+            .split(',')
+            .filter(linkString => linkString);
         imageLocations.forEach((img_link) => {
             articleHTML.append($('<div class="image-wrapper">').append($('<img>')
                 .addClass('trail-image')
@@ -119,7 +139,7 @@ function loadLocations() {
 
     function imageResizeOnClick() {
         let imageWrapper = $(this);
-        if(imageWrapper.width() === trailImagesCropSize) {
+        if (imageWrapper.width() === trailImagesCropSize) {
             imageWrapper
                 .css('width', '90%')
                 .css('height', 'auto')
@@ -138,5 +158,25 @@ function loadLocations() {
 
     }
 
-    requestLocations();
+    function toggleDOMElements() {
+        let targetIDArray = $(this).attr('relatedNodeID');
+        targetIDArray = targetIDArray.split(',');
+
+        for (let targetId of targetIDArray) {
+            let targetNode = $(`#${targetId}`);
+            if (!targetNode.find('li').length) {
+                switch (targetId) {
+                    case mountainLocationsListId:
+                        requestLocations(mountainsUrl, mountainList);
+                        break;
+                    case municipalityLocationsListId:
+                        requestLocations(municipalityUrl, municipalityList);
+                        break;
+                }
+            }
+
+            targetNode
+                .toggle();
+        }
+    }
 }
